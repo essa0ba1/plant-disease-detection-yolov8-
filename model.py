1d268093b74c56c7427f7fdba32a033624724b31
Bakhil Aissa <86269792+essa0ba1@users.noreply.github.com>
2024-02-09 15:41:39 +0100

    delete model.py

--- model.py
+++
@@ -1,6 +0,0 @@
-
-from ultralytics import YOLO
-
-model = YOLO("yolov8m.pt")
-
-results = model.train(data="/content/drive/MyDrive/Plants Diseases Detection and Classification.v12i.yolov8/data.yaml",epochs=50,lr0=0.0005,augment=True)
